14ceb565fedd04b0334b0408c63934af1362ba76
Jesus <jesustorde@gmail.com>
2018-09-13 17:40:32 -0400

    upenn comparison good done

--- upenn/mpl_ros/planning_ros_utils/src/planning_ros_utils/mesh_sampling.cpp
+++ upenn/mpl_ros/planning_ros_utils/src/planning_ros_utils/mesh_sampling.cpp
@@ -58,14 +58,15 @@
 
 #define PCL_VIS 0
 
-inline double uniform_deviate(int seed) {
+inline double uniform_deviate(int seed)
+{
   double ran = seed * (1.0 / (RAND_MAX + 1.0));
   return ran;
 }
 
-inline void randomPointTriangle(float a1, float a2, float a3, float b1,
-                                float b2, float b3, float c1, float c2,
-                                float c3, Eigen::Vector4f &p) {
+inline void randomPointTriangle(float a1, float a2, float a3, float b1, float b2, float b3, float c1, float c2,
+                                float c3, Eigen::Vector4f &p)
+{
   float r1 = static_cast<float>(uniform_deviate(rand()));
   float r2 = static_cast<float>(uniform_deviate(rand()));
   float r1sqr = sqrtf(r1);
@@ -86,13 +87,12 @@ inline void randomPointTriangle(float a1, float a2, float a3, float b1,
   p[3] = 0;
 }
 
-inline void randPSurface(vtkPolyData *polydata,
-                         std::vector<double> *cumulativeAreas, double totalArea,
-                         Eigen::Vector4f &p) {
+inline void randPSurface(vtkPolyData *polydata, std::vector<double> *cumulativeAreas, double totalArea,
+                         Eigen::Vector4f &p)
+{
   float r = static_cast<float>(uniform_deviate(rand()) * totalArea);
 
-  std::vector<double>::iterator low =
-      std::lower_bound(cumulativeAreas->begin(), cumulativeAreas->end(), r);
+  std::vector<double>::iterator low = std::lower_bound(cumulativeAreas->begin(), cumulativeAreas->end(), r);
   vtkIdType el = vtkIdType(low - cumulativeAreas->begin());
 
   double A[3], B[3], C[3];
@@ -102,13 +102,13 @@ inline void randPSurface(vtkPolyData *polydata,
   polydata->GetPoint(ptIds[0], A);
   polydata->GetPoint(ptIds[1], B);
   polydata->GetPoint(ptIds[2], C);
-  randomPointTriangle(float(A[0]), float(A[1]), float(A[2]), float(B[0]),
-                      float(B[1]), float(B[2]), float(C[0]), float(C[1]),
-                      float(C[2]), p);
+  randomPointTriangle(float(A[0]), float(A[1]), float(A[2]), float(B[0]), float(B[1]), float(B[2]), float(C[0]),
+                      float(C[1]), float(C[2]), p);
 }
 
 void uniform_sampling(vtkSmartPointer<vtkPolyData> polydata, size_t n_samples,
-                      pcl::PointCloud<pcl::PointXYZ> &cloud_out) {
+                      pcl::PointCloud<pcl::PointXYZ> &cloud_out)
+{
   polydata->BuildCells();
   vtkSmartPointer<vtkCellArray> cells = polydata->GetPolys();
 
@@ -116,7 +116,8 @@ void uniform_sampling(vtkSmartPointer<vtkPolyData> polydata, size_t n_samples,
   std::vector<double> cumulativeAreas(cells->GetNumberOfCells(), 0);
   size_t i = 0;
   vtkIdType npts = 0, *ptIds = NULL;
-  for (cells->InitTraversal(); cells->GetNextCell(npts, ptIds); i++) {
+  for (cells->InitTraversal(); cells->GetNextCell(npts, ptIds); i++)
+  {
     polydata->GetPoint(ptIds[0], p1);
     polydata->GetPoint(ptIds[1], p2);
     polydata->GetPoint(ptIds[2], p3);
@@ -128,7 +129,8 @@ void uniform_sampling(vtkSmartPointer<vtkPolyData> polydata, size_t n_samples,
   cloud_out.width = static_cast<pcl::uint32_t>(n_samples);
   cloud_out.height = 1;
 
-  for (i = 0; i < n_samples; i++) {
+  for (i = 0; i < n_samples; i++)
+  {
     Eigen::Vector4f p;
     randPSurface(polydata, &cumulativeAreas, totalArea, p);
     cloud_out.points[i].x = p[0];
@@ -137,8 +139,8 @@ void uniform_sampling(vtkSmartPointer<vtkPolyData> polydata, size_t n_samples,
   }
 }
 
-sensor_msgs::PointCloud
-convertToCloud(const pcl::PointCloud<pcl::PointXYZ> &msg) {
+sensor_msgs::PointCloud convertToCloud(const pcl::PointCloud<pcl::PointXYZ> &msg)
+{
   sensor_msgs::PointCloud2 cloud2;
   pcl::toROSMsg(msg, cloud2);
   sensor_msgs::PointCloud cloud;
@@ -154,10 +156,10 @@ using namespace pcl::console;
 int default_number_samples = 100000;
 float default_leaf_size = 0.01f;
 
-void printHelp(int, char **argv) {
+void printHelp(int, char **argv)
+{
   print_info("Params are:\n");
-  print_info(
-      "                     -n_samples X      = number of samples (default: ");
+  print_info("                     -n_samples X      = number of samples (default: ");
   print_value("%d", default_number_samples);
   print_info(")\n");
   print_info("                     -leaf_size X  = the XYZ leaf size for the "
@@ -167,7 +169,8 @@ void printHelp(int, char **argv) {
 }
 
 /* ---[ */
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   print_info("Convert a CAD model to a point cloud using uniform sampling. For "
              "more information, use: %s -h\n",
              argv[0]);
@@ -187,22 +190,25 @@ int main(int argc, char **argv) {
   printf("n_samples: %d, leaf_size: %.3f, save_pcd: %d\n", SAMPLE_POINTS_, leaf_size, save_pcd);
 
   // Parse the command line arguments for .ply and PCD files
-  std::string stlFileName;
-  n.param("file", stlFileName, std::string(""));
+  std::string stlFileName = "/home/jtorde/Downloads/"
+                            "willowgarage_visual2m.stl";
+  // n.param("file", stlFileName, std::string(""));
 
   vtkSmartPointer<vtkPolyData> polydata1 = vtkSmartPointer<vtkPolyData>::New();
 
-  if (stlFileName.compare(stlFileName.size() - 3, 3, "stl") == 0) {
+  if (stlFileName.compare(stlFileName.size() - 3, 3, "stl") == 0)
+  {
     pcl::PolygonMesh mesh;
     pcl::io::loadPolygonFileSTL(stlFileName, mesh);
     pcl::io::mesh2vtk(mesh, polydata1);
-  } else {
+  }
+  else
+  {
     ROS_WARN("Fail to open the file %s", stlFileName.c_str());
     return 0;
   }
   // make sure that the polygons are triangles!
-  vtkSmartPointer<vtkTriangleFilter> triangleFilter =
-      vtkSmartPointer<vtkTriangleFilter>::New();
+  vtkSmartPointer<vtkTriangleFilter> triangleFilter = vtkSmartPointer<vtkTriangleFilter>::New();
 #if VTK_MAJOR_VERSION < 6
   triangleFilter->SetInput(polydata1);
 #else
@@ -210,14 +216,12 @@ int main(int argc, char **argv) {
 #endif
   triangleFilter->Update();
 
-  vtkSmartPointer<vtkPolyDataMapper> triangleMapper =
-      vtkSmartPointer<vtkPolyDataMapper>::New();
+  vtkSmartPointer<vtkPolyDataMapper> triangleMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
   triangleMapper->SetInputConnection(triangleFilter->GetOutputPort());
   triangleMapper->Update();
   polydata1 = triangleMapper->GetInput();
 
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_1(
-      new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_1(new pcl::PointCloud<pcl::PointXYZ>);
   uniform_sampling(polydata1, SAMPLE_POINTS_, *cloud_1);
 
   // Voxelgrid
@@ -228,15 +232,14 @@ int main(int argc, char **argv) {
   pcl::PointCloud<pcl::PointXYZ>::Ptr res(new pcl::PointCloud<pcl::PointXYZ>);
   grid_.filter(*res);
 
-  ros::Publisher cloud_pub =
-      n.advertise<sensor_msgs::PointCloud>("cloud", 1, true);
+  ros::Publisher cloud_pub = n.advertise<sensor_msgs::PointCloud>("cloud", 1, true);
   sensor_msgs::PointCloud cloud = convertToCloud(*res);
-  cloud.header.frame_id = "map";
+  cloud.header.frame_id = "world";
   cloud_pub.publish(cloud);
 
   ROS_INFO("Cloud published!");
 
-  if(save_pcd)
+  if (save_pcd)
   {
     pcl::io::savePCDFileASCII("tmp.pcd", *res);
     ROS_INFO("Cloud saved!");
@@ -244,8 +247,5 @@ int main(int argc, char **argv) {
 
   ros::spin();
 
-
   return 0;
 }
-
-
