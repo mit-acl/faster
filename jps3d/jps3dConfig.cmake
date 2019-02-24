aa885c908d33f6129dc454edc870d3622abc9eb9
Jesus <jtorde@mit.edu>
2019-02-23 20:11:08 -0500

    minor

--- jps3d/jps3dConfig.cmake
+++ jps3d/jps3dConfig.cmake
@@ -8,5 +8,8 @@ get_filename_component(JPS3D_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 set(JPS3D_INCLUDE_DIRS "${JPS3D_CMAKE_DIR}/../../../include")
 
 set(JPS3D_LIBRARIES
-  "${JPS3D_CMAKE_DIR}/../../../lib/libjps_lib.so"
-  "${JPS3D_CMAKE_DIR}/../../../lib/libdmp_lib.so")
+  "${JPS3D_CMAKE_DIR}/../../../lib/libjps_lib.so")
+
+#set(JPS3D_LIBRARIES
+#  "${JPS3D_CMAKE_DIR}/../../../lib/libjps_lib.so"
+#  "${JPS3D_CMAKE_DIR}/../../../lib/libdmp_lib.so")
